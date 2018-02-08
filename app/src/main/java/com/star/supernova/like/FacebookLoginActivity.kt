17d6d9d45815fee1f0d440b9d0e3dc2c18fd9d8c
Joseph Nasr <J.nsr@hotmail.com>
2018-02-08 20:47:11 +0200

    Initial project

---
+++ app/src/main/java/com/star/supernova/like/FacebookLoginActivity.kt
@@ -0,0 +1,13 @@
+package com.star.supernova.like
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.CallbackManager
+
+class FacebookLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook_login)
+    }
+}
